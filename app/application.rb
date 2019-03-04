215027487d7c03dfa513f75c676398dc88db676f
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-04 21:06:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find{|i| i.name == item_name}
-      if i.name
-      resp.write item.price
-    else 
-      
+        if i.name
+         resp.write item.price
+       else 
+          resp.write "Route not found"
+          resp.status = 404
+        end 
     else 
       resp.write "Route not found"
       resp.status = 404
