a12d973bf2b4fb4e0cd077d4680ed1ad3f522cab
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-04 21:05:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,15 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
- 
       item_name = req.path.split("/items/").last 
       item = @@items.find{|i| i.name == item_name}
- 
+      if i.name
       resp.write item.price
     else 
       
+    else 
+      resp.write "Route not found"
+      resp.status = 404
     end
  
     resp.finish
