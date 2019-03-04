9fcfe797a3b39a986a75b2ebd04e8f7a1940c329
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-04 21:09:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find {|i| i.name == item_name}
-        if item.name
+        if @@items.include?(item)
          resp.write item.price
        else 
           resp.write "Route not found"
